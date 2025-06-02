0b283e8d579ff2f83d51c5ec186e938d7679e0fc
imShakil <mh.ice.iu@gmail.com>
2025-06-02 10:50:25 +0600

    updated pipeline

--- deploy-with-ecs/terraform/run_container_app.sh
+++ deploy-with-ecs/terraform/run_container_app.sh
@@ -3,5 +3,5 @@ apt-get update -y
 apt-get install curl -y
 curl -fsSL https://get.docker.com -o get-docker.sh
 sh get-docker.sh
-docker pull imshakil/react-test:latest
-docker run -d -p 80:3000 --name react-test imshakil/react-test:latest
+docker pull imshakil/flask-test:latest
+docker run -d -p 80:3000 --name react-test imshakil/flask-test:latest
